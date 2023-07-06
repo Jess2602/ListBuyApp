1eba50052e3531caf100f412473742dac8591501
TonyVyron <antoniofuentesjara@gmail.com>
2023-07-06 13:21:17 -0600

    FirebaseAuth
    FirebaseAuth para el login

--- app/src/main/java/com/example/listbuyapp/Login.kt
+++ app/src/main/java/com/example/listbuyapp/Login.kt
@@ -1,27 +1,188 @@
 package com.example.listbuyapp
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri.Builder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.listbuyapp.databinding.ActivityLoginBinding
 import com.example.listbuyapp.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
     lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+<<<<<<< Updated upstream
         binding.SingIn.setOnClickListener {
             // Aquí puedes agregar el código para pasar al MainActivity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+=======
+        setUp()
+        session()
+
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val uid = prefs.getString("uid", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null&& uid != null) {
+            showHome(email, uid ,ProviderType.valueOf(provider))
+>>>>>>> Stashed changes
         }
+    }
+
+    private fun setUp() {
         binding.SingUp.setOnClickListener {
+<<<<<<< Updated upstream
             // Aquí puedes agregar el código para pasar al MainActivity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+=======
+            if (binding.TextFieldEmail.text.toString()
+                    .isNotEmpty() && binding.TextFieldPass.text.toString().isNotEmpty()
+            ) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.TextFieldEmail.text.toString(),
+                    binding.TextFieldPass.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "",it.result?.user?.uid ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+
+            } else {
+                showAlertEmpty()
+            }
+        }
+        binding.SingIn.setOnClickListener {
+            if (binding.TextFieldEmail.text.toString()
+                    .isNotEmpty() && binding.TextFieldPass.text.toString().isNotEmpty()
+            ) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    binding.TextFieldEmail.text.toString(),
+                    binding.TextFieldPass.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "",it.result?.user?.uid ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+
+            } else {
+                showAlertEmpty()
+            }
+        }
+        binding.SingGoogle.setOnClickListener {
+            val googleconf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleconf)
+
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+>>>>>>> Stashed changes
         }
     }
+
+    private fun showHome(email: String,uid: String, provider: ProviderType) {
+        val homeIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("uid", uid)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+        finish()
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogCustom)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha Producido un error autenticando el Usuario")
+        builder.setPositiveButton("Aceptar") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.setTextColor(ContextCompat.getColor(this, R.color.botonbase))
+    }
+
+    private fun showAlertEx(error: String) {
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogCustom)
+        builder.setTitle("Error")
+        builder.setMessage(error)
+        builder.setPositiveButton("Aceptar") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.setTextColor(ContextCompat.getColor(this, R.color.botonbase))
+    }
+
+    private fun showAlertEmpty() {
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogCustom)
+        builder.setTitle("Error")
+        builder.setMessage("Los Campos están Vacíos")
+        builder.setPositiveButton("Aceptar") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.setTextColor(ContextCompat.getColor(this, R.color.botonbase))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener { signInTask ->
+                            if (signInTask.isSuccessful) {
+                                val firebaseUser = FirebaseAuth.getInstance().currentUser
+                                val uid = firebaseUser?.uid ?: ""
+                                showHome(account.email ?: "", uid, ProviderType.GOOGLE)
+                            } else {
+                                showAlert()
+                            }
+                        }
+
+                }
+            } catch (e: ApiException) {
+              showAlert()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/listbuyapp/MainActivity.kt
+++ app/src/main/java/com/example/listbuyapp/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.listbuyapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.listbuyapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +29,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
 
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val uid = bundle?.getString("uid")
+        val provider = bundle?.getString("provider")
+
+
+        val prefs =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("uid", uid)
+        prefs.putString("provider", provider)
+        prefs.apply()
 
         // Configurar Toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
 
         drawerLayout = binding.drawerLayout
         toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
@@ -40,13 +64,33 @@ class MainActivity : AppCompatActivity() {
                 R.id.home -> replaceFragment(homeFragment(), item.title.toString())
                 R.id.history -> replaceFragment(historyFragment(), item.title.toString())
                 R.id.categories -> replaceFragment(categoriesFragment(), item.title.toString())
+<<<<<<< Updated upstream
                 R.id.logOut -> finish()
                 R.id.left -> finish()
+=======
+                R.id.logOut -> {
+                    val prefs = getSharedPreferences(
+                        getString(R.string.prefs_file),
+                        Context.MODE_PRIVATE
+                    ).edit()
+                    prefs.clear()
+                    prefs.apply()
+                    FirebaseAuth.getInstance().signOut()
+                    onBackPressed()
+                }
+>>>>>>> Stashed changes
             }
             true
         }
 
-        // Reemplazar el fragmento inicial con homeFragment
+        val headerView: View = binding.navView.getHeaderView(0)
+        val emailc: TextView = headerView.findViewById(R.id.emailTextView)
+        val namec: TextView = headerView.findViewById(R.id.nameTextView)
+
+        val name = firebaseUser?.displayName.takeIf { it?.isNotEmpty() == true } ?: "User"
+        namec.text = name
+        emailc.text = email
+
         replaceFragment(homeFragment(), "Home")
     }
 
